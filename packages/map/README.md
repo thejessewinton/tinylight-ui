4816b2b26f50ae0f7578be5413b12a8c676c0b91
Jesse Winton <jesse@appwrite.io>
2025-05-23 23:26:17 -0400

    start on the map

--- packages/map/README.md
+++ packages/map/README.md
@@ -20,29 +20,67 @@ import { createMap } from "@tinylight-ui/map";
 
 ## Usage
 
+Create a map using the `createMap` function. The function takes an object with the following properties:
+- `width`: The width of the map in pixels.
+- `height`: The height of the map in pixels.
+- `markers`: An array of objects representing the markers to be placed on the map. Each object should have `lat` and `lng` properties, as well as an optional `size` property, which can be used to render custom markers of different sizes. `createMap` also accepts a generic type for additional properties on the marker. This can be useful for creating tooltips, or other customizations.
+- `mapSamples`: The number of samples to be taken from the map. This is used to create the map's points.
+
 ```typescript
 const { points, markers } = createMap({
   width: 1200,
   height: 600,
   markers: [
     {
-      latitude: 40.7128,
-      longitude: -74.006,
+      lat: 40.7128,
+      lng: -74.006,
+      size: 8,
     }, // New York
     {
 
-      latitude: 34.0522,
-      longitude: -118.2437,
+      lat: 34.0522,
+      lng: -118.2437,
     }, // Los Angeles
     {
-      latitude: 51.5074,
-      longitude: -0.1278,
+      lat: 51.5074,
+      lng: -0.1278,
     }, // London
     {
-      latitude: -33.8688,
-      longitude: 151.2093,
+      lat: -33.8688,
+      lng: 151.2093,
     }, // Sydney
   ],
   mapSamples: 4500,
 });
+```
+
+After creating a map, render it as an SVG however you'd like:
+
+```tsx
+const Map = () => {
+  return (
+    <svg viewBox={`0 0 1200 600`} className="h-full w-full">
+      {points.map((point) => {
+        return (
+          <circle
+            cx={point.x}
+            cy={point.y}
+            r={5}
+            fill="#eee"
+          />
+        );
+      })}
+      {markers.map((marker) => {
+        return (
+          <circle
+            cx={marker.x}
+            cy={marker.y}
+            r={5}
+            fill="#000"
+          />
+        );
+      })}
+    </svg>
+  );
+};
 ```
--- packages/map/src/types.ts
+++ packages/map/src/types.ts
@@ -1,29 +1,32 @@
 import { z } from 'zod';
 
 const polygonSchema = z.object({
-	type: z.literal('Polygon'),
-	coordinates: z.array(z.array(z.tuple([z.number(), z.number()])))
+  type: z.literal('Polygon'),
+  coordinates: z.array(z.array(z.tuple([z.number(), z.number()]))),
 });
 
 const multiPolygonSchema = z.object({
-	type: z.literal('MultiPolygon'),
-	coordinates: z.array(z.array(z.array(z.tuple([z.number(), z.number()]))))
+  type: z.literal('MultiPolygon'),
+  coordinates: z.array(z.array(z.array(z.tuple([z.number(), z.number()])))),
 });
 
-const geometrySchema = z.discriminatedUnion('type', [polygonSchema, multiPolygonSchema]);
+const geometrySchema = z.discriminatedUnion('type', [
+  polygonSchema,
+  multiPolygonSchema,
+]);
 
 export const geoJsonSchema = z.object({
-	type: z.literal('FeatureCollection'),
-	features: z.array(
-		z.object({
-			type: z.literal('Feature'),
-			id: z.string(),
-			properties: z.object({
-				name: z.string()
-			}),
-			geometry: geometrySchema
-		})
-	)
+  type: z.literal('FeatureCollection'),
+  features: z.array(
+    z.object({
+      type: z.literal('Feature'),
+      id: z.string(),
+      properties: z.object({
+        name: z.string(),
+      }),
+      geometry: geometrySchema,
+    })
+  ),
 });
 
 export type GeoJSON = z.infer<typeof geoJsonSchema>;
@@ -33,38 +36,35 @@ export type Geometry = z.infer<typeof geometrySchema>;
 export type GeoJsonFeature = z.infer<typeof geoJsonSchema>['features'][number];
 
 export interface Region {
-	lat: { min: number; max: number };
-	lng: { min: number; max: number };
+  lat: { min: number; max: number };
+  lng: { min: number; max: number };
 }
 
-export interface Marker {
-	lat: number;
-	lng: number;
-	size?: number;
-	city: string;
-	code: string;
-	available: boolean;
-}
+export type Marker<MarkerData> = {
+  lat: number;
+  lng: number;
+  size?: number;
+} & MarkerData;
 
-export interface CreateMapOptions {
-	height: number;
-	width: number;
-	countries?: string[];
-	mapSamples?: number;
-	region?: Region;
-	markers: Marker[];
+export interface CreateMapOptions<T> {
+  height: number;
+  width: number;
+  countries?: string[];
+  mapSamples?: number;
+  region?: Region;
+  markers: Marker<T>[];
 }
 export interface Pin {
-	lat: number;
-	lng: number;
+  lat: number;
+  lng: number;
 }
 
 export type Point = {
-	x: number;
-	y: number;
+  x: number;
+  y: number;
 };
 
 export interface BoundingBox {
-	lat: { min: number; max: number };
-	lng: { min: number; max: number };
+  lat: { min: number; max: number };
+  lng: { min: number; max: number };
 }
